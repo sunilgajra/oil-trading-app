93d15a42ed2e1b992666e4365d5dbe9876ec5287
sunilgajra <sunilgajra@gmail.com>
2026-05-14 16:45:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ═══════ SUPABASE CONFIG ═══════ */
-const SUPABASE_URL = "https://vrkilanytzftkpflqjh.supabase.co";
+const SUPABASE_URL = "https://vrkilanytzftkpfllqjh.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZya2lsYW55dHpmdGtwZmxscWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzQ5MTUsImV4cCI6MjA5MTgxMDkxNX0.D4BjNIYneCUkbiFnNR8MhsA9-yDYBYNR9Ha2AZemvXk";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
